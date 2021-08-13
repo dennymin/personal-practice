689c69434bcbaf60255bd133501c289792bbfc4c
Denny Min <denden223@gmail.com>
2021-08-12 17:48:03 -0700

    Started the note taking capabilities

--- study-screen/main.js
+++ study-screen/main.js
@@ -15,12 +15,41 @@ const flashing = setInterval(function() {
 const acquireTime = () => {
   const currentTime = new Date();
   currentTime.hours = currentTime.getHours();
-  if (currentTime.hours > 12) {
+  if (currentTime.hours >= 12 && currentTime.hours < 24) {
     currentTime.amPm = 'PM';
-    currentTime.hours = currentTime.hours - 12;
   } else {
     currentTime.amPm = 'AM';
   }
+  if (currentTime.hours > 12) {
+    currentTime.hours = currentTime.hours - 12;
+  }
   currentTime.minutes = currentTime.getMinutes();
+  if (currentTime.minutes.toString().length < 2) {
+    currentTime.minutes = '0' + currentTime.minutes;
+  }
   return currentTime;
 }
+
+const $EntriesContainer = document.querySelector('.entries-container');
+const $NewEntryButton = document.querySelector('.new-entry-button');
+
+
+const createDomElement = (type, variousClasses, someText) => {
+  const newDom = document.createElement(type);
+  newDom.className = variousClasses;
+  newDom.textContent = someText;
+  return newDom;
+}
+
+const newEntryClicked = () => {
+  const newEntryDiv = createDomElement('div', 'justify-content-center', '');
+  if (newEntryDiv.textContent !== '') {
+    $EntriesContainer.append(newEntryDiv);
+    console.log('created!')
+  } else {
+    console.log('not created!')
+    return;
+  }
+}
+
+$NewEntryButton.addEventListener('click', newEntryClicked)
