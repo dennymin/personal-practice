142deb168855089d6860384a91b87e3d37fcfd5d
Denny Min <denden223@gmail.com>
2021-09-27 11:50:52 -0700

    Got rid of the gift icon that I was originally using in place of a font awesome icon. Made some edits to the photos as some of them had a lot of whitespace. Wrapped the content in an anchor tag so that if the page was clicked, it would take the user to the right website. - Denny Min

--- acquirecrowd-email-creative/main.js
+++ acquirecrowd-email-creative/main.js
@@ -1,17 +1,48 @@
-const $offerContainer = document.querySelector('.offer-container')
 const $prizeName = document.querySelector('.prize-name');
 const $offerMainText = document.querySelector('.offer-main-text')
 const $prizeImg = document.querySelector('.prize-image')
 
-$offerContainer.addEventListener('click', (event) => {
-  console.log(window.location)
-})
+const prizes = [
+  {
+    name: 'Airpod Pros',
+    nameExtended: 'pair of Apple Airpod Pros',
+    imageSource: 'images/airpod-pros.png',
+    productId: 1
+  },
+  {
+    name: 'Apple Watch',
+    nameExtended: 'Apple Watch',
+    imageSource: 'images/apple-watch2.png',
+    productId: 2
+  }, {
+    name: 'Dyson Animal V8',
+    nameExtended: 'Dyson Animal V8 cordless vacuum engineered for homes with pets',
+    imageSource: 'images/dyson2.png',
+    productId: 3
+  }, {
+    name: 'EcoFlow',
+    nameExtended: 'EcoFlow Portable Power Station',
+    imageSource: 'images/ecoflow2.png',
+    productId: 4
+  }, {
+    name: 'MacBook Pro',
+    nameExtended: 'MacBook Pro',
+    imageSource: 'images/laptop.png',
+    productId: 5
+  }, {
+    name: 'Oura Ring',
+    nameExtended: 'Oura Ring that monitors sleep, readiness, and activity',
+    imageSource: 'images/ring.png',
+    productId: 6
+  }, {
+    name: 'Purple Pillow',
+    nameExtended: 'Purple Pillow with optimal head and neck support',
+    imageSource: 'images/purple-pillow.png',
+    productId: 7
+  }
+]
 
-const prize = {
-  name: 'Airpod Pros',
-  nameExtended: 'pair of Apple Airpod Pros',
-  imageSource: 'images/apple-watch.png'
-}
+const prize = prizes[0];
 
 const prizeNameTextContent = prize.name;
 $prizeName.textContent = prizeNameTextContent
